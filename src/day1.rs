281b7e5a0a94625fc2d82155bdce39bfd8de9c8f
Ritik Mishra <ritik.mishra314@gmail.com>
2020-12-01 21:05:25 -0600

    day 1

---
+++ src/day1.rs
@@ -0,0 +1,39 @@
+
+pub fn part1(input: &str) -> i32 {
+    let nums: Vec<i32> = convert_string_to_numbers(input);
+
+    for i in &nums {
+        for j in &nums {
+            if (*i + *j) == 2020 {
+                return *i * *j;
+            }
+        }
+    }
+    return -1;
+}
+
+
+pub fn part2(input: &str) -> i32 {
+    let nums: Vec<i32> = convert_string_to_numbers(input);
+
+    for i in &nums {
+        for j in &nums {
+            for k in &nums {
+                if (*i + *j + *k) == 2020 {
+                    return *i * *j * *k;
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+
+fn convert_string_to_numbers(stringified_nums: &str) -> Vec<i32> {
+    let mut nums: Vec<i32> = Vec::new();
+
+    for number_str in stringified_nums.split_whitespace() {
+        nums.push(number_str.parse::<i32>().unwrap())
+    }
+    return nums;
+}
