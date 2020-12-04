37d64b3fa617670f979411e03ec9d9e6c82d10e8
Ritik Mishra <ritik.mishra314@gmail.com>
2020-12-04 00:43:24 -0600

    day 4 completed

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     // day1::day1_main();
     // day2::day2_main()
-    day3::day3_main();
-
+    // day3::day3_main();
+    day4::day4_main();
 }
