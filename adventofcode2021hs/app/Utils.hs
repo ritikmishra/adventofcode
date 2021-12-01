171a92eb72741aa7f50e539a4f1ecd881a76f3b0
Ritik Mishra <ritik.mishra314@gmail.com>
2021-12-01 00:25:14 -0600

    change splitAtNextNewline to be tail recursive

--- adventofcode2021hs/app/Utils.hs
+++ adventofcode2021hs/app/Utils.hs
@@ -1,20 +1,22 @@
 module Utils where
 
-
 splitAtNextNewline :: String -> (String, String)
-splitAtNextNewline s =
-    case s of
-        "" -> ("", "")
-        '\n':rest -> ("", rest)
-        x:xs -> let (first, rest) = splitAtNextNewline xs in
-                (x:first, rest)
+splitAtNextNewline = splitAtNextNewlineTailRec ""
+  where
+    splitAtNextNewlineTailRec :: String -> String -> (String, String)
+    splitAtNextNewlineTailRec acc s =
+      case s of
+        "" -> (acc, "")
+        '\n' : rest -> (acc, rest)
+        -- hopefully ++ is tail recursive
+        x : xs -> splitAtNextNewlineTailRec (acc ++ [x]) xs
 
 splitAllNewlines :: String -> [String]
 splitAllNewlines s =
-    let (first, rest) = splitAtNextNewline s in
-        case rest of
-            "" -> [first]
-            words -> first:splitAllNewlines rest
+  let (first, rest) = splitAtNextNewline s
+   in case rest of
+        "" -> [first] -- nothing else to split
+        words -> first : splitAllNewlines rest
 
 stringToIntList :: String -> [Int]
 stringToIntList s = map read (splitAllNewlines s)
